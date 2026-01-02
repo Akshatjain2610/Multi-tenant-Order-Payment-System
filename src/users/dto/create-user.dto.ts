78517fc3694384abf7724c383f62b8da67b724e9
= <=>
2026-01-02 15:17:38 +0530

    feat: Enhance CreateUserDto with ApiProperty decorators for better Swagger documentation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,9 +1,18 @@
 import { IsEmail, IsString, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
+  @ApiProperty({
+    description: "Email for creating user",
+    example: "example@example.com",    
+  })
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    description: "min 6 digit string password which contains UPPERCASE, LOWERCASE, NUMERIC VALUES",
+    example: "Abcde@123"
+  })
   @IsString()
   @MinLength(6)
   password: string;
